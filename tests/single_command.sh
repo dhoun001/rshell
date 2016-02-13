177b4b58cb866b18fcd64c981343658d28e7c2b4
Shawn Wu <swu038@ucr.edu>
2016-02-12 21:51:29 -0800

    Listed and tested the single commands

---
+++ tests/single_command.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#test single commands 
+
+#testing the ls command:
+    ls
+    
+#testing with one parameter:
+    ls -a
+    
+#testing with two parameters:
+    ls -a -l 
+
+#testing with three parameters:
+    ls -a -l -h 
+
+#testing the echo command:
+    echo 
+
+#testing the mkdir command: 
+    mkdir trial 
+    #ls to check: 
+        ls 
+
+#testing the vim command: 
+    vim trial.cpp 
+
+#testing an unknown command: 
+    asdf
+    *fails when try to exit after 
+
+#testing with integers: 
+    122343
+
+#testing with symbols: 
+    !@$ 
+
+
+ 
+
+
